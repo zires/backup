d35d5bb05b397186419939c722ad3129bd9f1c44
Michael van Rooijen <meskyanichi@gmail.com>
2011-10-31 13:12:15 +0100

    Added experimental support for symlink following to Backup::Archive.

--- lib/backup/archive.rb
+++ lib/backup/archive.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
+##
+# Require the pathname Ruby library when Backup::Archive is loaded
+require "pathname"
+
 module Backup
   class Archive
     include Backup::CLI
@@ -21,11 +25,16 @@ module Backup
     attr_accessor :archive_path
 
     ##
+    # Optional flag to tell Backup::Archive to follow all the symlinks in the @paths variable
+    attr_accessor :follow_symlinks
+
+    ##
     # Takes the name of the archive and the configuration block
     def initialize(name, &block)
-      @name     = name.to_sym
-      @paths    = Array.new
-      @excludes = Array.new
+      @name            = name.to_sym
+      @paths           = Array.new
+      @excludes        = Array.new
+      @follow_symlinks = false
 
       instance_eval(&block)
     end
@@ -46,14 +55,35 @@ module Backup
     # Archives all the provided paths in to a single .tar file
     # and places that .tar file in the folder which later will be packaged
     def perform!
-      @archive_path = File.join(TMP_PATH, TRIGGER, 'archive')
-      mkdir(archive_path)
+      prepare!
 
       Logger.message("#{ self.class } started packaging and archiving #{ paths.map { |path| "\"#{path}\""}.join(", ") }.")
       run("#{ utility(:tar) } -c -f '#{ File.join(archive_path, "#{name}.tar") }' #{ paths_to_exclude } #{ paths_to_package }", :ignore_exit_codes => [1])
     end
 
   private
+
+    ##
+    # Prepares the Backup::Archive object before actually
+    # performing the archving process
+    #
+    # If @follow_symlinks is set to true, then Backup::Archive will
+    # search through every directory (recursively) for symlinks and append
+    # their absolute path to the array of @paths to package.
+    def prepare!
+      @archive_path = File.join(TMP_PATH, TRIGGER, 'archive')
+      mkdir(archive_path)
+
+      if follow_symlinks
+        paths.each do |path|
+          collect_symlinks(Pathname.new(path)).each do |symlink|
+            @paths << symlink.realpath
+          end
+        end
+      end
+
+      @paths.uniq!
+    end
 
     ##
     # Returns a "tar-ready" string of all the specified paths combined
@@ -69,6 +99,24 @@ module Backup
       if excludes.any?
         excludes.map{ |e| "--exclude='#{e}'" }.join(" ")
       end
+    end
+
+    ##
+    # Collects symlinks in all directories (recursively) for the given path
+    def collect_symlinks(pathname)
+      return Array.new unless pathname.directory?
+
+      pathname.children.map do |entry|
+        symlink = if entry.symlink?
+          entry
+        end
+
+        directory = if entry.directory?
+          collect_symlinks(entry)
+        end
+
+        Array.new([symlink, directory])
+      end.flatten.compact
     end
   end
 end
